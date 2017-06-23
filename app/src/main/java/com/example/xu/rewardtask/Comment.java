bac5ceb806fc945ed68eb1d228503cc8ccd3a293
JeremieCHN <274488038@qq.com>
2017-06-23 21:06:33 +0800

    增加评论功能

---
+++ app/src/main/java/com/example/xu/rewardtask/Comment.java
@@ -0,0 +1,72 @@
+package com.example.xu.rewardtask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Comment {
+    static String ADOPTED = "true";
+    static String UNADOPTED = "false";
+
+    private String missionUsername, missionName, comment, username, isAdopted;
+    private Date date;
+
+    public Comment(String missionUsername_, String missionName_, String comment_, String username_, Date date_) {
+        this.username = username_;
+        this.missionName = missionName_;
+        this.comment = comment_;
+        this.missionUsername = missionUsername_;
+        this.date = date_;
+        isAdopted = "false";
+    }
+
+    public Comment(JSONObject jsonObject) throws JSONException, ParseException {
+        missionUsername = jsonObject.getString("Username");
+        missionName = jsonObject.getString("Missionname");
+        comment = jsonObject.getString("Comment");
+        username = jsonObject.getString("Commentor");
+        isAdopted = jsonObject.getString("IsAdopt");
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date = format.parse(jsonObject.getString("Date").substring(0, 19));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getMissionUsername() {
+        return missionUsername;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isAdopt() {
+        return !isAdopted.equals(UNADOPTED);
+    }
+
+    public void setAdopt() {
+        isAdopted = ADOPTED;
+    }
+
+    public String toPostParams() {
+        return "username=" + missionUsername
+                + "&mission=" + missionName
+                + "&commentor=" + username
+                + "&comment=" + comment;
+    }
+}
--- app/src/main/java/com/example/xu/rewardtask/MissionDetailActivity.java
+++ app/src/main/java/com/example/xu/rewardtask/MissionDetailActivity.java
@@ -66,12 +66,189 @@ public class MissionDetailActivity extends AppCompatActivity {
 
     void initView() {
         Intent intent = getIntent();
+        missionPublisher = intent.getStringExtra("UserName");
+        missionName = intent.getStringExtra("MissionName");
+
+        PublisherTimeTV = (TextView) findViewById(R.id.MissionDetail_UserTime);
+        tip = (TextView) findViewById(R.id.MissionDetail_Tip);
+        IsCompletedTip = (TextView) findViewById(R.id.MissionDetail_IsCompleted);
+
+        commentEdit = (EditText) findViewById(R.id.MissionDetail_CommentEdit);
+        commentEdit.setImeOptions(EditorInfo.IME_ACTION_SEND);
+        commentEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    new sendComment().execute(
+                            new Comment(missionPublisher, missionName,
+                                    commentEdit.getText().toString(),
+                                    CurrentUser.getInstance().getUserName(),
+                                    new Date(System.currentTimeMillis())));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // 未登录的话直接不显示评论编辑的部分
+        if (!CurrentUser.getInstance().isLogin())
+            findViewById(R.id.MissionDetail_CommentEditPart).setVisibility(View.GONE);
+
+        Button button = (Button) findViewById(R.id.MissionDetail_ReleaseButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, missionPublisher);
+                Log.i(TAG, missionName);
+                Log.i(TAG, commentEdit.getText().toString());
+                Log.i(TAG, CurrentUser.getInstance().getUserName());
+
+                new sendComment().execute(new Comment(missionPublisher, missionName,
+                        commentEdit.getText().toString(),
+                        CurrentUser.getInstance().getUserName(),
+                        new Date(System.currentTimeMillis())));
+            }
+        });
+        ((TextView) findViewById(R.id.MissionDetail_MissionName)).setText(missionName);
+        PublisherTimeTV.setText(missionPublisher);
+
         if (intent.getStringExtra("Date") != null) {
             Date date = new Date(intent.getStringExtra("Date"));
             DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
             PublisherTimeTV.setText(missionPublisher + "发布于" + dateFormat.format(date));
         }
+
+        initListView();
     }
+
+    private List<Comment> CommentList_Data;
+    private CommentAdapter adapter;
+
+    void initListView() {
+        commentLV = (ListView) findViewById(R.id.MissionDetail_Comment);
+        commentLV.setVisibility(View.GONE);
+
+        CommentList_Data = new LinkedList<>();
+        adapter = new CommentAdapter(CommentList_Data);
+        commentLV.setAdapter(adapter);
+
+        commentLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                // 未登陆点击不会有任何反应
+                if (!CurrentUser.getInstance().isLogin())
+                    return;
+
+                final Comment item = CommentList_Data.get(position);
+
+                // 登陆者是任务发布者 且 任务赏金未被领取才会有采纳
+                if (item.getMissionUsername().equals(CurrentUser.getInstance().getUserName())
+                        && IsCompletedTip.getText().equals("此任务的赏金尚未被领取")) {
+                    Log.i(TAG, item.getMissionUsername());
+                    Log.i(TAG, CurrentUser.getInstance().getUserName());
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MissionDetailActivity.this);
+                    LayoutInflater inflater = getLayoutInflater();
+                    View dialogView = inflater.inflate(R.layout.activity_mission_detail_dialog, null);
+                    builder.setView(dialogView);
+                    final AlertDialog dialog = builder.create();
+
+                    Button adoptButton = (Button) dialogView.findViewById(R.id.MissionDetail_AdoptComment);
+                    adoptButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (dialog.isShowing())
+                                dialog.cancel();
+
+                            new AdoptComment().execute(position);
+                        }
+                    });
+
+                    Button replyButton = (Button) dialogView.findViewById(R.id.MissionDetail_ReplyComment);
+                    replyButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            commentEdit.setText("回复 " + item.getUsername() + ": ");
+                            commentEdit.setSelection(5+item.getUsername().length());
+                            commentEdit.requestFocus();
+
+                            if (dialog.isShowing())
+                                dialog.cancel();
+                        }
+                    });
+
+                    dialog.show();
+                } else {
+                    commentEdit.setText("回复 " + item.getUsername() + ": ");
+                    commentEdit.setSelection(5+item.getUsername().length());
+                    commentEdit.requestFocus();
+                }
+            }
+        });
+    }
+
+    class CommentAdapter extends BaseAdapter {
+        private List<Comment> list = null;
+
+        CommentAdapter(List<Comment> list_) {
+            list = list_;
+        }
+
+        @Override
+        public int getCount() {
+            if (list == null)
+                return 0;
+            else
+                return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            if (list == null)
+                return null;
+            else
+                return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup parent) {
+            View convertView;
+            ViewHolder viewHolder;
+            if (view == null) {
+                convertView = LayoutInflater.from(MissionDetailActivity.this).inflate(R.layout.activity_mission_detail_list_item, null);
+                viewHolder = new ViewHolder();
+                viewHolder.commenter = (TextView) convertView.findViewById(R.id.MissionDetail_Comment_Item_UserName);
+                viewHolder.commentDetail = (TextView) convertView.findViewById(R.id.MissionDetail_Comment_Item_Comment);
+                viewHolder.commentDate = (TextView) convertView.findViewById(R.id.MissionDetail_Comment_Item_Date);
+                convertView.setTag(viewHolder);
+            } else {
+                convertView = view;
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.commenter.setText(list.get(i).getUsername());
+            viewHolder.commentDetail.setText(list.get(i).getComment());
+            DateFormat format = new SimpleDateFormat("yy-MM-dd HH:mm");
+            viewHolder.commentDate.setText(format.format(list.get(i).getDate()));
+
+            if (list.get(i).isAdopt())
+                viewHolder.commentDetail.setTextColor(getResources().getColor(R.color.colorRed));
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView commenter;
+            TextView commentDetail;
+            TextView commentDate;
+        }
+    }
+
     class getMissionDetailAsyncTask extends AsyncTask<Void, Void, String> {
 
         @Override
@@ -161,7 +338,258 @@ public class MissionDetailActivity extends AppCompatActivity {
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
+                new getCommentAsyncTask().execute();
             }
+        }
+    }
+
+    class getCommentAsyncTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpURLConnection connection = null;
+
+            try {
+                URL url = new URL("http://" + CurrentUser.IP + "/AndroidServer/getComment?mission=" +
+                        URLEncoder.encode(missionName, "UTF-8") + "&publisher=" +
+                        URLEncoder.encode(missionPublisher, "UTF-8"));
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(4000);
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                StringBuffer buffer = new StringBuffer();
+                InputStream is = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                reader.close();
+
+                Log.i(TAG, buffer.toString());
+
+                JSONArray array = new JSONArray(buffer.toString());
+                String statue = array.optJSONObject(0).getString("Status");
+                if (statue != null && statue.equals("Success")) {
+                    CommentList_Data.clear();
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject jsonObject = array.getJSONObject(i);
+                        CommentList_Data.add(new Comment(jsonObject));
+                    }
+                }
+                return statue;
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                return "InternetGG";
+            } catch (SocketTimeoutException e) {
+                e.printStackTrace();
+                return "InternetGG";
+            } catch (ConnectException e) {
+                e.printStackTrace();
+                return "InternetGG";
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return "Fail";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.equals("Fail")) {
+                Toast.makeText(MissionDetailActivity.this, "评论获取失败，请稍后再试", Toast.LENGTH_SHORT).show();
+            } else if (s.equals("InternetGG")) {
+                Toast.makeText(MissionDetailActivity.this, "网络错误，请稍后再试", Toast.LENGTH_SHORT).show();
+            } else if (s.equals("Empty")) {
+                tip.setText("暂无评论");
+            } else if (s.equals("Success")) {
+                tip.setVisibility(View.GONE);
+                commentLV.setVisibility(View.VISIBLE);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    class AdoptComment extends AsyncTask<Integer, Void, Integer> {
+
+        String MissionNameSend;
+        String MissionPublisherSend;
+        String CommentPublisherSend;
+        String DateSend;
+        String GoldSend;
+
+        @Override
+        protected void onPreExecute() {
+            try {
+                MissionNameSend = URLEncoder.encode(missionName, "UTF-8");
+                MissionPublisherSend = URLEncoder.encode(missionPublisher, "UTF-8");
+                GoldSend = moneyTV.getText().toString();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            HttpURLConnection connection = null;
+
+            try {
+                CommentPublisherSend = URLEncoder.encode(CommentList_Data.get(params[0]).getUsername(), "UTF-8");
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                DateSend = URLEncoder.encode(format.format(CommentList_Data.get(params[0]).getDate()), "UTF-8");
+
+                URL url = new URL("http://" + CurrentUser.IP + "/AndroidServer/adoptServlet?"
+                        + "mission=" + MissionNameSend
+                        + "&publisher=" + MissionPublisherSend
+                        + "&username=" + CommentPublisherSend
+                        + "&date=" + DateSend
+                        + "&gold=" + GoldSend);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(4000);
+                connection.setReadTimeout(4000);
+                connection.connect();
+
+                InputStream is = connection.getInputStream();
+                StringBuilder builder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+                JSONObject jsonObject = new JSONObject(builder.toString());
+                if (jsonObject.getString("Status") != null && jsonObject.getString("Status").equals("Success")) {
+                    return params[0];
+                } else {
+                    return null;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer c) {
+            if (c != null) {
+                IsCompletedTip.setText("此任务的赏金已经被领取");
+                CommentList_Data.get(c).setAdopt();
+                adapter.notifyDataSetChanged();
+                CurrentUser.getInstance().setMoney(CurrentUser.getInstance().getMoney() - Integer.parseInt(moneyTV.getText().toString()));
+            } else {
+                Toast.makeText(MissionDetailActivity.this, "采纳失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    class sendComment extends AsyncTask<Comment, Void, String> {
+
+        @Override
+        protected String doInBackground(Comment... params) {
+            Log.i(TAG, params[0].toPostParams() + " is ready to send");
+            HttpURLConnection connection;
+
+            try {
+                URL url = new URL("http://" + CurrentUser.IP + "/AndroidServer/sendComment");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+
+                Log.i(TAG, params[0].toPostParams());
+
+                connection.getOutputStream().write(params[0].toPostParams().getBytes());
+                connection.connect();
+
+                Log.i(TAG, Integer.toString(connection.getResponseCode()));
+
+                if (connection.getResponseCode() == 200) {
+                    StringBuilder builder = new StringBuilder();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null)
+                        builder.append(line);
+
+                    Log.i(TAG, "Response: " + builder.toString());
+                    return builder.toString();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        AlertDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MissionDetailActivity.this);
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_loading, null);
+
+            dialogView.findViewById(R.id.Dialog_Loading).setAnimation(AnimationUtils.loadAnimation(MissionDetailActivity.this, R.anim.roteting));
+            ((TextView) dialogView.findViewById(R.id.Dialog_Text)).setText("正在发布评论...");
+
+            builder.setView(dialogView);
+            dialog = builder.create();
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                @Override
+                public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
+                        return true;
+                    else
+                        return false;
+
+                }
+            });
+
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    String statusStr = jsonObject.getString("Status");
+                    String dateStr = jsonObject.getString("Date");
+                    if (statusStr != null && statusStr.equals("Success")) {
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+                        CommentList_Data.add(new Comment(missionPublisher, missionName, commentEdit.getText().toString(),
+                                CurrentUser.getInstance().getUserName(), format.parse(dateStr)));
+
+                        commentEdit.setText("");
+                        Toast.makeText(MissionDetailActivity.this, "发布成功", Toast.LENGTH_SHORT).show();
+                        if (dialog != null)
+                            dialog.cancel();
+                        new getCommentAsyncTask().execute();
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(MissionDetailActivity.this, "发布失败，请稍后再试", Toast.LENGTH_SHORT).show();
+            if (dialog != null)
+                dialog.cancel();
         }
     }
 
